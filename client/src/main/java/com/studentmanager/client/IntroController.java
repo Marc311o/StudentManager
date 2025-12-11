0f84f9a1bf9fe6306f475d4bc792263af3d57cd2
Jerzy Andrusiewicz <jurek.andrusiewicz@gmail.com>
2025-12-11 16:54:57 +0100

    code refactorization
    cleanup

--- client/src/main/java/com/studentmanager/client/IntroController.java
+++ client/src/main/java/com/studentmanager/client/IntroController.java
@@ -1,30 +1,21 @@
 package com.studentmanager.client;
 
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
-
 import javafx.event.ActionEvent;
 import java.io.IOException;
 import java.util.Objects;
 
 public class IntroController {
 
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
-
-    @FXML
-    private Button goBtn;
-
     public void setGoBtnRelease(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/management.fxml")));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+
         stage.setScene(scene);
         stage.centerOnScreen();
         stage.show();
--- client/src/main/java/com/studentmanager/client/ManagementController.java
+++ client/src/main/java/com/studentmanager/client/ManagementController.java
@@ -14,17 +14,11 @@ import java.util.Objects;
 
 public class ManagementController {
 
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
-
-    @FXML
-    private Button goBackBtn;
-
     public void goBackToMenuBtnRelease(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/intro.fxml")));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+
         stage.setScene(scene);
         stage.centerOnScreen();
         stage.show();
